a8e9e40d5c8ca4d00c868bfa173758aa3f0c2f95
Daniil <kegazani@practice.energy>
2025-12-06 18:24:00 +0300

    fixes

--- cassandra-entrypoint.sh
+++ cassandra-entrypoint.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 set -e
 
-if [ -f /etc/cassandra/jvm.options ]; then
-    sed -i 's/-XX:+UseConcMarkSweepGC/#-XX:+UseConcMarkSweepGC/' /etc/cassandra/jvm.options
-    sed -i 's/-XX:CMSInitiatingOccupancyFraction=/#-XX:CMSInitiatingOccupancyFraction=/' /etc/cassandra/jvm.options
-    sed -i 's/-XX:+UseCMSInitiatingOccupancyOnly/#-XX:+UseCMSInitiatingOccupancyOnly/' /etc/cassandra/jvm.options
+JVM_OPTS_FILE="/etc/cassandra/jvm.options"
+
+if [ -f "$JVM_OPTS_FILE" ]; then
+    sed -i.bak 's/^[^#]*-XX:+UseConcMarkSweepGC/#-XX:+UseConcMarkSweepGC/' "$JVM_OPTS_FILE"
+    sed -i.bak 's/^[^#]*-XX:CMSInitiatingOccupancyFraction=/#-XX:CMSInitiatingOccupancyFraction=/' "$JVM_OPTS_FILE"
+    sed -i.bak 's/^[^#]*-XX:+UseCMSInitiatingOccupancyOnly/#-XX:+UseCMSInitiatingOccupancyOnly/' "$JVM_OPTS_FILE"
+    sed -i.bak 's/^[^#]*-XX:+CMSParallelRemarkEnabled/#-XX:+CMSParallelRemarkEnabled/' "$JVM_OPTS_FILE"
+    sed -i.bak 's/^[^#]*-XX:+CMSParallelInitialMarkEnabled/#-XX:+CMSParallelInitialMarkEnabled/' "$JVM_OPTS_FILE"
     
-    if ! grep -q "UseG1GC" /etc/cassandra/jvm.options; then
-        echo "-XX:+UseG1GC" >> /etc/cassandra/jvm.options
-        echo "-XX:MaxGCPauseMillis=200" >> /etc/cassandra/jvm.options
-        echo "-XX:InitiatingHeapOccupancyPercent=45" >> /etc/cassandra/jvm.options
+    if ! grep -q "^[^#]*-XX:+UseG1GC" "$JVM_OPTS_FILE"; then
+        echo "" >> "$JVM_OPTS_FILE"
+        echo "# G1GC settings (replacing CMS)" >> "$JVM_OPTS_FILE"
+        echo "-XX:+UseG1GC" >> "$JVM_OPTS_FILE"
+        echo "-XX:MaxGCPauseMillis=200" >> "$JVM_OPTS_FILE"
+        echo "-XX:InitiatingHeapOccupancyPercent=45" >> "$JVM_OPTS_FILE"
     fi
 fi
 
@@ -18,6 +24,6 @@ if [ -f /usr/local/bin/docker-entrypoint.sh ]; then
 elif [ -f /docker-entrypoint.sh ]; then
     exec /docker-entrypoint.sh "$@"
 else
-    exec /bin/bash -c "cassandra -f"
+    exec cassandra -f
 fi
 
